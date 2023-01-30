22d493e5b397ffbf3829bcacb3584258ae72f10f
Mohammad Hossein Amirpanahi <mhap75@yahoo.com>
2023-01-30 15:17:27 +0330

    fix share performance bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-    $('.content_footer').on('click', '#shrbtn' ,function () {
+    $('.content_footer').on('click', '#shrbtn', function () {
 
         if ($('.social-share').css('display') === 'none') {
-            $('.social-share').fadeIn(600)
+            $('.social-share').fadeIn(600);
             $('.social-share').css('display', 'flex').animate({
                 opacity: 1
             }, 650);
@@ -12,7 +12,7 @@ $(document).ready(function () {
             $('.social-share_mobile').fadeOut(600);
         }
         if ($('.social-share_mobile').css('display') === 'none') {
-            $('.social-share_mobile').show(600)
+            $('.social-share_mobile').show(600);
             $('.social-share_mobile').css('display', 'flex').animate({
                 bottom: 0
             }, 650);
@@ -20,6 +20,13 @@ $(document).ready(function () {
             $('.social-share').fadeOut(600);
             $('.social-share_mobile').fadeOut(600);
         }
+
+    });
+
+    $('.body').on("mousedown", function () {
+
+        $('.social-share, .social-share_mobile').fadeOut(600);
+
     });
 
 });
